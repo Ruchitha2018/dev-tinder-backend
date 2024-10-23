541c71adc4e448e57e36215da41ad5252c9ebcb0
Ruchitha <rudeshpande@paypal.com>
2024-10-23 19:10:38 +0530

    Validation in User Schema

--- src/app.js
+++ src/app.js
@@ -1,28 +1,35 @@
 const express = require("express");
 const app = express();
-const connectDB = require('../src/config/database')
+const connectDB = require("../src/config/database");
 const User = require("./models/user");
 
-app.post('/signup', async (req, res) => {
-    const userObj = {
-      firstName: 'Ruchitha',
-      lastName: 'Deshpande',
-      emailId: 'abc@gmail.com',
-      password: '12345abc'
-    }
-    const user = new User(userObj);
-    await user.save();
-    res.send("User Added")
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+  await user.save();
+  res.send("User Added");
+});
+
+app.get("/user", async (req,res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.find({ emailId: userEmail})
+    res.send(user)
+  }
+  catch(err) {
+    console.log(err);
+  }
 })
 
-
-
-connectDB().then(() => {
-  console.log("Database connected")
-  app.listen(8080, () => {
-    console.log('server is running in 8080 port')
-})
-})
-.catch(err => {
-  console.log("Database cannot be connected", err)
-})
+connectDB()
+  .then(() => {
+    console.log("Database connected");
+    app.listen(8080, () => {
+      console.log("server is running in 8080 port");
+    });
+  })
+  .catch((err) => {
+    console.log("Database cannot be connected", err);
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,44 @@
 const { mongoose } = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String
+      type: String,
+      required: true,
     },
     lastName: {
-        type: String
+      type: String,
     },
     emailId: {
-        type: String
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String
+      type: String,
     },
     age: {
-        type: String
+      type: String,
     },
     gender: {
-        type: String
-    }
-})
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "This is a default value",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
